b377bffd3c192f4de779b746eab2e3be7e15f8e1
sushantdev-git <sushantkumar6700@gmail.com>
2022-05-19 06:47:16 +0530

    added firebase functions...

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import LoginPage from "./Pages/LoginPage/LoginPage";
 import { Routes, Route, Navigate } from "react-router-dom"; 
 import HomePage from "./Pages/Home/HomePage";
 
+import {auth} from './firebase';
+import {useAuthState} from 'react-firebase-hooks/auth'
+
 function App() {
+
+  const [user, loading, error] = useAuthState(auth);
+
   return (
     <Routes>
-      <Route path='/home' exact element={<HomePage/>}></Route>
-      <Route path='/login' exact element={<LoginPage/>}></Route>
+      <Route path='/home' exact element={user ? <HomePage/> : <Navigate to={'/login'}/>}></Route>
+      <Route path='/login' exact element={user ? <HomePage/> : <LoginPage/>}></Route>
       <Route path='/' exact element={<Navigate to={'/login'}/>}></Route>
     </Routes>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAUivw0DrbiE5JfosgMMjbhyoVxeSgUb5o",
   authDomain: "login-app-12ef8.firebaseapp.com",
@@ -16,6 +28,54 @@ const firebaseConfig = {
   measurementId: "G-MT8MT8F6Z0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
